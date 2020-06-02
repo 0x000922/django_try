1cc5c358786b439a0b4a02c04fc4becaf0d2375b
0x000922 <creater3494@protonmail.com>
2020-06-02 23:52:32 +0530

    admin files

--- rdbms/main/admin.py
+++ rdbms/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Films,Customers
+from .models import Films as Film ,Customers as customer,Reserved_seat as seats_reserved, Screenings as screening  
 # Register your models here.
-admin.site.register(Films)
-admin.site.register(Customers)
+admin.site.register(Film)
+admin.site.register(customer)
+admin.site.register(seats_reserved)
+admin.site.register(screening)
